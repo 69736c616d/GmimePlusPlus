61ed516c48005d9b45238a8362a50b8d68d13c57
Islam YASAR <islamyasar@yandex.com>
2018-06-17 02:47:48 +0100

    main added

---
+++ main.cpp
@@ -0,0 +1,53 @@
+#include <gmimepp.hpp>
+#include <iostream>
+
+using namespace std;
+
+
+
+int main(int argc, char const *argv[])
+{
+
+  //while(true)
+  //{
+      GmimePP gpp(argv[1]);
+
+      gpp.init();
+      cout << "From: " << gpp.getHeader("FROM") << endl;
+      cout << "To: " << gpp.getHeader("To") << endl;
+      cout << "Subject: " << gpp.getHeader("Subject") << endl;
+      cout << "Message-ID: " << gpp.getHeader("Message-ID") << endl;
+
+      if (gpp.addHeader("header1", "value1") == -1)
+          cout <<"-1" << endl;
+
+      gpp.setHeader("header1", "value2");
+
+
+      std::vector<SHeaderValue> allrcpts;
+      gpp.getAllRecipients(allrcpts);
+
+      for (auto &rcpt : allrcpts)
+          cout << "allrcpt" << rcpt.name << "-" << rcpt.value << endl;
+
+      allrcpts.clear();
+
+      /*
+          GMIME_RECIPIENT_TYPE_TO,
+          GMIME_RECIPIENT_TYPE_CC,
+          GMIME_RECIPIENT_TYPE_BCC
+      */
+      gpp.getRecipientsByType(GMIME_RECIPIENT_TYPE_TO, allrcpts);
+
+      for (auto &rcpt : allrcpts)
+          cout << "rcptbytype" <<rcpt.name << "-" << rcpt.value << endl;
+
+      cout << gpp.getFromAdress() << endl;
+      sleep(0.1);
+  //}
+
+      gpp.saveAttachments("/home/iyasar/gmimetest/GmimePlusPlus");
+
+
+  return 0;
+}
